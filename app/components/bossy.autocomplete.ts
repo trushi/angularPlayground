c63183cc7d6a22a9b471902538022095898904d0
rthakkar <rthakkar@mail.csuchico.edu>
2016-05-05 15:50:39 -0700

    chooseSuggestion and filter function updated

--- app/components/bossy.autocomplete.ts
+++ app/components/bossy.autocomplete.ts
@@ -43,6 +43,58 @@ export class BossyAutocomplete {
                 scope.query = suggestion;
                 scope.updateSuggestions(suggestion);
             };
+                   createMatrix(x,y){
+             let i, mat = new Array(x);
+                for (i = 0; i < x; i++) {
+                    mat[i] = new Array(y);
+                }
+                return mat;
+            };
+            
+            filterStartsWith(words, query, caseInsensitive){
+            let compare;
+            if (caseInsensitive) {
+            compare = function (w) {
+                return w.toLowerCase().startsWith(query.toLowerCase());
+                };
+            }
+            else {
+            compare = function (w) {
+                return w.startsWith(query)
+                };
+            }
+            return words.filter(compare);
+        };
+        function levDist(str1, str2 : string) {
+        let i, j, mat = utility.createMatrix(str1.length + 1, str2.length + 1);
+        // More readable if strings are 1-indexed
+        // lower case for case-insensitive matching
+        str1 = ' ' + str1.toLowerCase();
+        str2 = ' ' + str2.toLowerCase();
+        // Initialize first row values to edit distance from empty string
+        for (i = 0; i < str1.length; i++) {
+            mat[i][0] = i;
+        }
+        // Initialize first column values to edit distance from empty string
+        for (j = 0; j < str2.length; j++) {
+            mat[0][j] = j;
+        }
+        // Flood fill mat with edit distances between substrs of str1/str2
+            for (j = 1; j < str2.length; j++) {
+                for (i = 1; i < str1.length; i++) {
+                    if (str1[i] === str2[j]) {
+                        mat[i][j] = mat[i - 1][j - 1];
+                }
+                else {
+                    mat[i][j] = Math.min(mat[i - 1][j] + 1,    // a deletion
+                        mat[i][j - 1] + 1,    // an insertion
+                        mat[i - 1][j - 1] + 1); // a substitution
+                }
+            }
+        }
+        return mat[str1.length - 1][str2.length - 1];
+    }
+
 
 /*
 
